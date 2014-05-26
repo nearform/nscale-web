ca7e1fa9bf7224e12ab01e03a945ad178bd9071f
Chico Charlesworth <chico.charlesworth@googlemail.com>
2014-05-26 11:18:34 +0100

    Fixed logic on home (aka systems) page

--- app/scripts/controllers/home.js
+++ app/scripts/controllers/home.js
@@ -1,12 +1,25 @@
 'use strict';
 
-angular.module('nfdWebApp').controller('HomeCtrl', function ($scope, $http, api) {
+angular.module('nfdWebApp').controller('HomeCtrl', function ($scope, $http, $window, auth, api) {
 
-	var user = {id:"1"};
+  function init(user) {
+    $scope.user = user;
+    api.get('/systems', user, function(systems){
+      $scope.systems = systems;
+      $scope.show = true;
+    });
+  }
 
-	api.get('/systems', user, function(systems){
-    $scope.systems = systems;
-    $scope.show = true;
-  });
+  $scope.user = null;
+  $scope.show = false;
+  auth.checkLoggedIn(function(user) {init(user);});
+  $scope.btn_signout = function() {auth.logout();}
+
+  $scope.delete = function(systemId) {
+    api.del('/system/' + systemId, $scope.user, function(result){
+      console.log('System deleted');
+      $window.location.href = $window.location.href;
+    });
+  }
 
 });
